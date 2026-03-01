ea915357a2f8038c6913b89c617df6c8112f0b73
HimanthaTharidu <eg255691@engug.ruh.ac.lk>
2026-03-02 02:07:42 +0530

    Install and setup Tailwind css & create login form and auth interfaces

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,11 @@
 <script setup lang="ts">
-
+import LoginForm from "@/components/LoginForm.vue"
 </script>
 
 <template>
-  
+    
+<button class="bg-blue-500 text-white px-4 py-2 rounded">click</button>
+
 </template>
 
 <style scoped>
---
+++ src/components/LoginForm.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+<div class="flex items-center justify-center min-h-screen bg-gray-50 p-4">
+    <div>
+        <input type="text" placeholder="User Name">
+        <input type="password" placeholder="Password">
+        <button>Submit</button>
+    </div>
+</div>
+</template>
+
+<style scoped>
+
+</style>
