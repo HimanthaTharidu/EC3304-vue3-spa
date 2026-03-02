844a685d07e7bd2003dde692cbac64f5eb2c1c2b
HimanthaTharidu <eg255691@engug.ruh.ac.lk>
2026-03-02 18:15:28 +0530

    implement auth logic with async fetch and localStorage

--- src/components/LoginForm.vue
+++ src/components/LoginForm.vue
@@ -2,15 +2,45 @@
 import { ref } from 'vue'; 
 const username = ref('');
 const password = ref('');
-
+const errorMessage = ref('');
 const handleSubmit = () => { console.log('Form submmited with :', username.value , password.value);};
+
+const handleLogin = async () => {
+    try{
+        errorMessage.value = '';
+
+        const response = await fetch('https://dummyjson.com/auth/login', {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({
+                
+                username: username.value,
+                password: password.value,
+                //expiresInMins:30, // optional, defaults to 60
+            }),
+            //credentials: 'include' // Include cookies (e.g., accessToken) in the request
+            })
+            const data = await response.json();
+
+            if(!response.ok){
+                throw new Error(data.message || "login Failed")
+            }
+            localStorage.setItem('userToken', data.token);
+            console.log("Login Sucessful! Token stored.")
+    }
+    catch (error : any){
+        errorMessage.value = error.message;
+        console.log('Login error:', error)
+    }
+};
+
 </script>
 
 <template>
 <div class="flex items-center justify-center min-h-screen bg-gray-50 p-4">
     <div class="w-full max-w-md rounded-lg bg-white p-8 shadow-xl">
         <h2 class="mb-6 text-center text-2xl font-bold text-gray-800">Login</h2>
-        <form @submit.prevent="handleSubmit" class="space-y-6">
+        <form @submit.prevent="handleLogin" class="space-y-6">
             <div class="mb-6">
                 <label class="mb-2 block text-sm font-medium text-gray-700">Username</label>
                 <input
@@ -32,6 +62,10 @@ const handleSubmit = () => { console.log('Form submmited with :', username.value
             class="w-full rounded-md bg-blue-600 py-2 font-semibold text-white transition duration-200 hover:bg-blue-700 active:bg-blue-800">
             Sign In
             </button>
+            <p v-if="errorMessage" 
+            class="mt-4 text-sm font-medium text-red-600 bg-red-50 p-2 rounded border border-red-200">
+        ⚠️ {{ errorMessage }}
+            </p>
         </form>
         <p class="mt-4 text-center text-sm text-gray-600">
         Don't have an account? <a href="#" class="text-blue-600 hover:underline">Sign up</a>
